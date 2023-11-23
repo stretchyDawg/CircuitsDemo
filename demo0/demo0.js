64dc64c99f3d92013e5e790f285351dd980cf6e7
Christian <cmmorgado53@gmail.com>
2023-11-23 03:16:05 -0500

    removed save and load, and added coordinate tracking

---
+++ demo0/demo0.js
@@ -0,0 +1,78 @@
+console.log("page loaded")
+// let screenLog = document.getElementById("screen-log")
+// document.addEventListener("mousemove", logKey);
+// function logKey(e) {
+//     screenLog.innerText = `
+//       Screen X/Y: ${e.screenX}, ${e.screenY}
+//       Client X/Y: ${e.clientX}, ${e.clientY}`;
+//   }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const schematicEditor = document.getElementById('schematic-editor');
+    const components = [];
+    const wires = [];
+    
+    // State
+    let isDrawing = false;
+    let startWire = null;
+    
+    schematicEditor.addEventListener('mousedown', handleMouseDown);
+    schematicEditor.addEventListener('mousemove', handleMouseMove);
+    schematicEditor.addEventListener('mouseup', handleMouseUp);
+
+    function handleMouseDown(event) {
+        const component = createComponent(event);
+        components.push(component);
+
+        startWire = createWire(event);
+        isDrawing = true;
+    }
+
+    function handleMouseMove(event) {
+        let screenLog = document.getElementById("screen-log")
+        screenLog.innerText = `
+              Screen X/Y: ${event.screenX}, ${event.screenY}
+              Client X/Y: ${event.clientX}, ${event.clientY}
+              Offset X/Y: ${event.offsetX}, ${event.offsetY}`;
+        if (isDrawing) {
+            updateWire(event.offsetX, event.offsetY);
+        }
+    }
+
+    function handleMouseUp(event) {
+        if (isDrawing) {
+            isDrawing = false;
+            const endWire = createWire(event.offsetX, event.offsetY);
+            connectComponents(startWire, endWire);
+            wires.push({ start: startWire, end: endWire });
+        }
+    }
+
+    function createComponent(event) {
+        const containerRect = document.querySelector('.container').getBoundingClientRect();
+        const component = document.createElement('div');
+        component.className = 'component';
+        component.style.left = (event.offsetX - containerRect.left) + 'px';
+        component.style.top = (event.offsetY - containerRect.top) + 'px';
+        schematicEditor.appendChild(component);
+        return component;
+    }
+
+    function createWire(event) {
+        
+        const containerRect = document.querySelector('.container').getBoundingClientRect();
+        const wire = document.createElement('div');
+        wire.className = 'wire';
+        wire.style.left = (event.offsetX - containerRect.left) + 'px';
+        wire.style.top = (event.offsetY - containerRect.top) + 'px';
+        schematicEditor.appendChild(wire);
+        return wire;
+    }
+
+    function updateWire(x, y) {
+        if (startWire) {
+            startWire.style.width = x - startWire.offsetLeft + 'px';
+            startWire.style.height = y - startWire.offsetTop + 'px';
+        }
+    }
+});
